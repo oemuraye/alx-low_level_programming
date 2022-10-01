8280a8322459e406e09f860bc1a2bfbb18aef87a
oemuraye <oemuraye@gmail.com>
2022-09-30 18:07:44 -0700

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,22 +11,30 @@
  **/
 int main(int argc, char *argv[])
 {
-	int sum, i, j;
+	int sum = 0, i;
 
-	sum = 0;
-	for (i = 1; i < argc; i++)
+	if (argc > 1)
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
+		for (i = 1; i < argc; i++)
 		{
-			if (!isdigit(argv[i][j]))
+			int b;
+			char *str;
+
+			str = argv[i];
+			for (b = 0; str[b] != '\0'; b++)
 			{
-				printf("Error\n");
-				return (1);
+				if (str[b] < 48 || str[b] > 57)
+				{
+					printf("Error\n");
+					return (1);
+				}
 			}
 		}
+	}
+	for (i = 1; i < argc; i++)
+	{
 		sum += atoi(argv[i]);
 	}
-
 	printf("%d\n", sum);
 	return (0);
 }
