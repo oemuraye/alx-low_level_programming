a2bd36e5c079e4fc96a0905d0e05cdf14acd5800
Emuraye Pius 0 <76441493+oemuraye@users.noreply.github.com>
2022-10-25 11:53:35 +0100

    Update lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -13,9 +13,9 @@
  */
 typedef struct listint_s
 {
-	int n;
-	struct listint_s *next;
-} listint_t;
+	void *p;
+	struct listp_s *next;
+} listp_t;
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
